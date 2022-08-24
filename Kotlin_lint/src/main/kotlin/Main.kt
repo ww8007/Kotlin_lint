a5b1f8bf2d84d4492eacb562a4fb18b2bf1971e5
장동현 <54137044+ww8007@users.noreply.github.com>
2022-08-24 22:00:16 +0900

    chore: Fix tab_width to 2

--- Kotlin_lint/src/main/kotlin/Main.kt
+++ Kotlin_lint/src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
     println("hi")
+    println("hi")
 
     fun printSum(a: Int, b: Int) {
         println("sum of $a and $b is ${a + b}")
